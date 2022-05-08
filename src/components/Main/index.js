d3a727bfc6e8b3d76b0400a028406e368055e09a
Lucas Golle <lucasgolle@pop-os.localdomain>
2022-05-08 04:11:32 -0300

    feat: create skils list developer

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,18 +1,57 @@
-import {Container, Back} from "./styles"
+import { Container, Back, TitulosHabilidades, HabilidadesBack, HabilidadesFront } from "./styles";
+import SpinnerIcon from "@rsuite/icons/legacy/Spinner";
 
-
-export const Main = () =>{
-
-    return(
-        <>
-        
-        <Container>
-            <div>
-            <h3>Front-End</h3>
-            <Back>Back-End</Back>
-            </div>
+export const Main = () => {
+  return (
+    <>
+    {/* <GlobalContainer> */}
+      <Container>
+        <TitulosHabilidades>
+          <h3>Front-End</h3>
+          <Back>Back-End</Back>
+        </TitulosHabilidades>
+        <HabilidadesBack>
+        <div>
+            <h3>Aprendendo Python</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+          <div>
+            <h3>Aprendendo MongoDB</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+          <div>
+            <h3>Aprendendo SQL</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+          <div>
+            <h3>Aprendendo APIs RESTful</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+        </HabilidadesBack>
+        <HabilidadesFront>
+        <div>
+            <h3>React</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+          <div>
+            <h3>JavaScript</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+          <div>
+            <h3>CSS</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+          <div>
+            <h3>GIT</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+          <div>
+            <h3>TypeScript</h3>
+          <SpinnerIcon pulse style={{ fontSize: "1em" }} />
+          </div>
+        </HabilidadesFront>
         </Container>
-
-        </>
-    )
-}
+      {/* </GlobalContainer> */}
+    </>
+  );
+};
--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -1,32 +1,96 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-        width: 600px;
-        height: 600px;
-        border-radius: 50%;
-        margin: 5em auto;
-        border: 1px solid #000;
-        background: radial-gradient(circle at 50% 25%, black 5%, transparent 5%), radial-gradient(closest-side circle at 50% 25%, white 100%, transparent 100%),
-        radial-gradient(circle at 50% 75%, white 5%, transparent 5%),
-        radial-gradient(closest-side circle at 50% 75%, black 100%, transparent 100%),
-        linear-gradient(to right, white 50%, black 50%);
-        
-        div{
-          display: flex;
-          flex-direction: column;
-          height: 600px;
-          position: relative;
-          align-items: center;
-          color: #1C1C1C;
-          justify-content: space-around;
-        }
-
-        
+export const GlobalContainer = styled.div`
+        display: flex;
+        align-items: flex-end;
+        flex-direction: row;
 `
+
+export const Container = styled.div`
+  width: 600px;
+  height: 600px;
+  border-radius: 50%;
+  margin: 5em auto;
+  border: 1px solid #000;
+  background: radial-gradient(circle at 50% 25%, black 5%, transparent 5%),
+    radial-gradient(
+      closest-side circle at 50% 25%,
+      white 100%,
+      transparent 100%
+    ),
+    radial-gradient(circle at 50% 75%, white 5%, transparent 5%),
+    radial-gradient(
+      closest-side circle at 50% 75%,
+      black 100%,
+      transparent 100%
+    ),
+    linear-gradient(to right, white 50%, black 50%);
+
+`;
+
+export const TitulosHabilidades = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+  position: relative;
+  align-items: center;
+  color: #1c1c1c;
+  justify-content: space-around;
+`;
 
 export const Back = styled.h3`
+  color: #ffff;
+  margin-top: 160px;
+`;
+export const HabilidadesBack = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+  position: relative;
+  align-items: center;
+  color: #1c1c1c;
+  margin-top: -50%;
+  margin-left: 50%;
 
+  svg{
+          color: #ffff;
+  }
+
+  h3{
+        margin-right: 10px;
         color: #ffff;
-        margin-top: 160px;
-`
+  }
 
+  div {
+    display: flex;
+    flex-direction: row;
+    height: 40px;
+    width: 210px;
+    align-items: center;
+  }
+`;
+
+export const HabilidadesFront = styled.div`
+        display: flex;
+        flex-direction: column;
+        height: 600px;
+        position: relative;
+        align-items: center;
+        color: #1c1c1c;
+        margin-top: -130%;
+        margin-right: 40%;
+
+  
+  h3{
+        margin-right: 10px;
+        color: #1c1c1c;
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    height: 40px;
+    width: 210px;
+    align-items: center;
+  }
+`
