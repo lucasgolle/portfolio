eae702d02b09eb4b40537270c355769d55245b5a
Lucas <lucas_rodrigues1@yahoo.com.br>
2022-05-25 23:14:37 -0300

    new styled component about, mobile first

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -5,22 +5,18 @@ export const About = () => {
   return (
     <Container>
       <h1>Um pouco sobre mim</h1>
-      <div>
         <img src={profilePicture} alt="imagemPerfil" />
-        <p>
-          Olá, me chamo Lucas Golle. Sou estudante de Ciência da Computação e
+        <p>Olá, me chamo Lucas Golle. Sou estudante de Ciência da Computação e
           desenvolvimento web. Esse é meu portfolio, no qual pretendo atualizar
           e melhorar durante toda a minha carreira. Minha paixão pela
           programação vem desde de muito novo. Sempre trabalhei em áreas
           técnicas de empresas de comunicação (televisão e rádio) operando
-          softweres. Minha meta agora além de operar e entender, é estar do
-          outro lado, desenvolvendo essas ferramentas fantásticas.
-        </p>
-      </div>
-      <div>
+          softwares. Minha meta agora além de operar e entender, é estar do
+          outro lado, desenvolvendo essas ferramentas fantásticas.</p>
+        <div>
         <h2>Perfil</h2>
         <p>Apaixonado por tecnologia, games, Harry Potter.</p>
-      </div>
+        </div>
     </Container>
   );
 };
--- src/components/about/styles.js
+++ src/components/about/styles.js
@@ -1,3 +1,44 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 90vw;
+  justify-content: center;
+  margin-bottom: 4rem;
+
+  h1 {
+    text-align: center;
+    font-size: 1.4rem;
+    margin-top: 1rem;
+    margin-bottom: 4rem;
+  }
+
+  p {
+    font-size: 1rem;
+    text-align: left;
+    color: var(--gray-800);
+    letter-spacing: 0.1rem;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    text-align: left;
+  }
+
+  img {
+    width: 120px;
+    height: 120px;
+    border-radius: 100%;
+    margin: 0 auto;
+    margin-bottom: 1rem;
+    display: block;
+  }
+
+  h2 {
+    text-align: start;
+    font-size: 1.2rem;
+    margin-top: 1rem;
+  }
+`;
