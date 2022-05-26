e4a6fb597e8e4b38856efcfc89acc0cecc07ca24
Lucas <lucas_rodrigues1@yahoo.com.br>
2022-05-25 22:44:39 -0300

    update: new image profile about Folder select

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,9 +1,26 @@
-import {Container, MenuBar} from "./styles"
+import { Container } from "./styles";
+import profilePicture from "../../assets/pictureProfile.jpeg"
 
 export const About = () => {
-    return(
-        <Container>
-            Oi boi
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <h1>Um pouco sobre mim</h1>
+      <div>
+        <img src={profilePicture} alt="imagemPerfil" />
+        <p>
+          Olá, me chamo Lucas Golle. Sou estudante de Ciência da Computação e
+          desenvolvimento web. Esse é meu portfolio, no qual pretendo atualizar
+          e melhorar durante toda a minha carreira. Minha paixão pela
+          programação vem desde de muito novo. Sempre trabalhei em áreas
+          técnicas de empresas de comunicação (televisão e rádio) operando
+          softweres. Minha meta agora além de operar e entender, é estar do
+          outro lado, desenvolvendo essas ferramentas fantásticas.
+        </p>
+      </div>
+      <div>
+        <h2>Perfil</h2>
+        <p>Apaixonado por tecnologia, games, Harry Potter.</p>
+      </div>
+    </Container>
+  );
+};
