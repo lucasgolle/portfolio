9868af4e4886f1c9496164d3f0e91abbe6713fc2
Lucas <lucas_rodrigues1@yahoo.com.br>
2022-05-28 06:05:10 -0300

    update: new animation on main div

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -1,40 +1,50 @@
 import styled, { keyframes } from "styled-components";
 
-const animateYang = keyframes`
-    from{
-        transform:rotate(0deg);
+// const animateYang =
+//     from{
+//         transform:rotate(0deg);
+//     }
+
+//     to{
+//         transform:rotate(360deg);
+//     }
+
+// `;
+
+const animateYing = keyframes`
+    0%{
+      transform: translateY(0);
     }
 
-    to{
-        transform:rotate(360deg);
+    100%{
+      transform: translateY(25px);
     }
 
 `;
+
 export const Container = styled.div`
-
-    width: 320px;
-    height: 320px;
-    border-radius: 50%;
-    max-width: 100vw;
-    margin: 5em auto;
-    border: 1px solid #000;
-    background: radial-gradient(circle at 50% 25%, black 5%, transparent 5%),
-      radial-gradient(
-        closest-side circle at 50% 25%,
-        white 100%,
-        transparent 100%
-      ),
-      radial-gradient(circle at 50% 75%, white 5%, transparent 5%),
-      radial-gradient(
-        closest-side circle at 50% 75%,
-        black 100%,
-        transparent 100%
-      ),
-      linear-gradient(to right, white 50%, black 50%);
-      box-shadow: 14px 14px 20px #111111;
-      animation: ${animateYang} 1s;
-      margin-bottom: 20rem;
-
+  width: 320px;
+  height: 320px;
+  border-radius: 50%;
+  max-width: 100vw;
+  margin: 5em auto;
+  border: 1px solid #000;
+  background: radial-gradient(circle at 50% 25%, black 5%, transparent 5%),
+    radial-gradient(
+      closest-side circle at 50% 25%,
+      white 100%,
+      transparent 100%
+    ),
+    radial-gradient(circle at 50% 75%, white 5%, transparent 5%),
+    radial-gradient(
+      closest-side circle at 50% 75%,
+      black 100%,
+      transparent 100%
+    ),
+    linear-gradient(to right, white 50%, black 50%);
+  box-shadow: 14px 14px 20px #111111;
+  animation: ${animateYing} 1.8s infinite alternate;
+  margin-bottom: 20rem;
 
   @media (min-width: 768px) {
     width: 600px;
@@ -44,15 +54,13 @@ export const Container = styled.div`
 `;
 
 export const TitulosHabilidades = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 300px;
-    position: relative;
-    align-items: center;
-    color: #1c1c1c;
-    justify-content: space-around;
-
-
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  position: relative;
+  align-items: center;
+  color: #1c1c1c;
+  justify-content: space-around;
 
   @media (min-width: 768px) {
     height: 600px;
@@ -64,23 +72,22 @@ export const Back = styled.h3`
   margin-top: 160px;
 `;
 export const HabilidadesBack = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 300px;
-    font-size: 0.6rem;
-    position: relative;
-    align-items: flex-start;
-    color: #ffff;
-    margin-top: -44%;
-    margin-left: 55%;
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  font-size: 0.6rem;
+  position: relative;
+  align-items: flex-start;
+  color: #ffff;
+  margin-top: -44%;
+  margin-left: 55%;
 
-    div {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }
-  
-  
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
   svg {
     color: #ffff;
     margin-top: 5px;
@@ -90,9 +97,8 @@ export const HabilidadesBack = styled.div`
     margin-right: 10px;
     color: #ffff;
     margin-top: 5px;
-    }
+  }
 
-  
   @media (min-width: 768px) {
     display: flex;
     flex-direction: column;
@@ -111,31 +117,29 @@ export const HabilidadesBack = styled.div`
       align-items: center;
     }
     h3 {
-    margin-right: 10px;
-    color: #ffff;
+      margin-right: 10px;
+      color: #ffff;
     }
   }
 `;
 
 export const HabilidadesFront = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  font-size: 0.6rem;
+  position: relative;
+  align-items: flex-start;
+  color: black;
+  margin-top: -130%;
+  margin-left: 20%;
 
-display: flex;
-    flex-direction: column;
-    height: 300px;
-    font-size: 0.6rem;
-    position: relative;
-    align-items: flex-start;
-    color: black;
-    margin-top: -130%;
-    margin-left: 20%;
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
 
-    div {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }
-  
-  
   svg {
     color: #ffff;
     margin-top: 5px;
@@ -145,8 +149,7 @@ display: flex;
     margin-right: 10px;
     color: black;
     margin-top: 5px;
-    }
-
+  }
 
   @media (min-width: 768px) {
     height: 600px;
